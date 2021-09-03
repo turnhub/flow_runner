313340ea8441d7f0a94c0c6c5fc06873a3c412f5
Simon de Haan <simon@praekelt.org>
2021-09-03 10:25:11 +0200

    default packages

--- mix.exs
+++ mix.exs
@@ -4,9 +4,11 @@ defmodule FlowRunner.MixProject do
   def project do
     [
       app: :flow_runner,
-      version: "0.1.0",
+      version: String.trim(File.read!("VERSION")),
       elixir: "~> 1.12",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: LcovEx, output: "cover"],
       deps: deps()
     ]
   end
@@ -19,11 +21,17 @@ defmodule FlowRunner.MixProject do
     ]
   end
 
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["test/support", "lib"]
+  defp elixirc_paths(_), do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:mix_test_watch, "~> 1.1"},
+      {:credo, "~> 1.5"}
     ]
   end
 end
