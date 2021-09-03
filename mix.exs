c8a67bf8f8d537409926feb200dbf553ac5041a5
Simon de Haan <simon@praekelt.org>
2021-09-03 10:54:22 +0200

    pull in expression lib

--- mix.exs
+++ mix.exs
@@ -31,6 +31,7 @@ defmodule FlowRunner.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:credo, "~> 1.5"},
+      {:expression, "~> 0.2.1"},
       {:junit_formatter, "~> 3.1", only: [:test]},
       {:mix_test_watch, "~> 1.1"}
     ]
