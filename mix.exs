b97bebe1ddd8f0ac673c39ddf2b63e0b8157d614
Simon de Haan <simon@praekelt.org>
2021-09-23 11:49:22 +0200

    resolve some hex package dependency conflicts

--- mix.exs
+++ mix.exs
@@ -33,12 +33,12 @@ defmodule FlowRunner.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:credo, "~> 1.5"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
       {:expression, "~> 0.2.1"},
       {:junit_formatter, "~> 3.1", only: [:test]},
-      {:mix_test_watch, "~> 1.1"},
+      {:mix_test_watch, "~> 1.1", only: [:dev, :test], runtime: false},
       {:poison, "~> 5.0"}
     ]
   end
