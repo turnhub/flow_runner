b91febd898e63a5d95fb69098df940d5a9b343e1
Santiago Cardona <santiagocardo80@gmail.com>
2022-04-12 19:19:55 -0300

    fix(custom): behaviour implementation

--- lib/flow_runner/custom.ex
+++ lib/flow_runner/custom.ex
@@ -1,9 +1,33 @@
 defmodule FlowRunner.Custom do
   defmacro __using__(_opts) do
     quote do
-      defdelegate fetch_flow_by_uuid(container, uuid), to: FlowRunner.Spec.Container
-      defdelegate fetch_resource_by_uuid(container, uuid), to: FlowRunner.Spec.Container
-      defoverridable(fetch_flow_by_uuid: 2, fetch_resource_by_uuid: 2)
+      @behaviour FlowRunner
+
+      defdelegate compile(json), to: FlowRunner
+      defdelegate compile(blocks_module, json), to: FlowRunner
+      defdelegate compile!(json), to: FlowRunner
+      defdelegate compile!(blocks_module, json), to: FlowRunner
+
+      @impl FlowRunner
+      defdelegate create_context(container, flow_uuid, language, mode, vars \\ %{}),
+        to: FlowRunner
+
+      @impl FlowRunner
+      defdelegate evaluate_expression(expression, context), to: FlowRunner
+
+      @impl FlowRunner
+      defdelegate evaluate_expression_block(expression, context), to: FlowRunner
+
+      @impl FlowRunner
+      defdelegate evaluate_next_block(container, flow, next_block, context), to: FlowRunner
+
+      @impl FlowRunner
+      defdelegate next_block(container, context, user_input \\ nil), to: FlowRunner
+
+      @impl FlowRunner
+      defdelegate fetch_flow_by_uuid(container, uuid), to: FlowRunner
+
+      defoverridable(fetch_flow_by_uuid: 2)
     end
   end
 end
--- test/flow_runner_custom_test.exs
+++ test/flow_runner_custom_test.exs
@@ -1,23 +1,37 @@
-defmodule FlowRunner.CustomTest.Runner do
-  use FlowRunner.Custom
-
-  def fetch_flow_by_uuid(_container, _uuid) do
-    # custom_lookup_in_db(uuid)
-  end
-end
-
 defmodule FlowRunner.CustomTest do
   use ExUnit.Case, async: true
   import FlowRunner.Test.Utils
   doctest FlowRunner.Custom
 
   # instruct this test module to use the custom runner
-  @moduletag flow_runner: FlowRunner.CustomTest.Runner
+  @moduletag flow_runner: FlowRunner.Test.Custom
 
   setup :with_flow_loader!
 
   @tag flow: "test/basic.flow"
   test "compile a flow", %{container: container} do
     assert container
+  end
+
+  @tag flow: "test/basic.flow"
+  test "run a flow", %{container: container} do
+    {:ok, context} =
+      FlowRunner.create_context(
+        container,
+        "62d0084d-e88f-48c3-ac64-7a15855f0a43",
+        "eng",
+        "TEXT"
+      )
+
+    {:ok, _context, _, output} = FlowRunner.next_block(container, context)
+    assert output.prompt.value == "welcome to this block"
+    assert output.prompt.modes == ["TEXT"]
+    assert output.prompt.content_type == "TEXT"
+  end
+
+  test "fetch a flow", %{flow_runner: flow_runner} do
+    {:ok, flow} = apply(flow_runner, :fetch_flow_by_uuid, [nil, "some-id"])
+
+    assert flow.name == "test santiago"
   end
 end
