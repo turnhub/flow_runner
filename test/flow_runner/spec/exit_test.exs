8e974ea722673d2de23e346bf57726ff7fec4fc7
Federico Meini <federico@turn.io>
2024-03-26 19:42:36 +0100

    Add test

--- test/flow_runner/spec/exit_test.exs
+++ test/flow_runner/spec/exit_test.exs
@@ -17,4 +17,29 @@ defmodule FlowRunner.Spec.ExitTest do
              %{vars: %{"ref_Buttons_7bef16" => "2"}}
            )
   end
+
+  test "Exit matching a string containing a boolean-ish value" do
+    assert Exit.evaluate(
+             %Exit{
+               uuid: "dff8ba17-d73d-4607-91de-dca8b2ed8630",
+               name: "Exit for Text_4dd50b",
+               destination_block: "be115dce-1f85-515c-9494-95bf3f00e87d",
+               semantic_label: nil,
+               test: "ref_Buttons_7bef16 == \"True\"",
+               default: false,
+               config: %{},
+               vendor_metadata: %{}
+             },
+             %{
+               vars: %{
+                 "ref_Buttons_7bef16" => %{
+                   "__value__" => "True",
+                   "index" => 0,
+                   "label" => "True",
+                   "name" => "True"
+                 }
+               }
+             }
+           )
+  end
 end
