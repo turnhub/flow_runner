5f5b1bb4cc4c07d84dc6e162bd1d35c2394495bc
Simon de Haan <simon@turn.io>
2024-07-22 07:42:24 +0200

    wait for otel fixes

--- mix.exs
+++ mix.exs
@@ -63,7 +63,7 @@ defmodule FlowRunner.MixProject do
       {:jason, "~> 1.2"},
       {:junit_formatter, "~> 3.1", only: [:test]},
       {:mix_test_watch, "~> 1.1", only: [:dev, :test], runtime: false},
-      {:open_telemetry_decorator, "<=1.4.13 or >1.5.4"},
+      {:open_telemetry_decorator, "<=1.4.13 or >1.5.6"},
       {:version_tasks, "~> 0.12.0",
        only: [:dev], runtime: false, github: "turnhub/version_tasks"},
       {:vex, "~> 0.9.0"}
