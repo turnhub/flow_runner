1c76d5fd216bedabbebf1bb943055cd4a81e2e37
David Kang <davidkhk@gmail.com>
2025-03-17 16:48:27 -0300

    Upgrade deps

--- mix.exs
+++ mix.exs
@@ -57,7 +57,7 @@ defmodule FlowRunner.MixProject do
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:elixir_uuid, "~> 1.2"},
       {:excoveralls, "~> 0.10", only: :test},
-      {:expression, "~> 2.42.1"},
+      {:expression, "~> 2.43.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:finch, "~> 0.17"},
       {:iptools, "~> 0.0.5"},
