1734164d5c5da9cb0ba97fad20ac4b91d969997a
João Thallis <joaothallis@icloud.com>
2025-06-06 00:05:43 -0300

    refactor(simulator): simplify button parameter filtering logic
    Remove unused case

--- lib/flow_runner/simulator.ex
+++ lib/flow_runner/simulator.ex
@@ -562,13 +562,7 @@ defmodule FlowRunner.Simulator do
       template_components
       |> Enum.filter(&(&1.type == "button"))
       |> Enum.map(fn button -> Map.get(button, :parameters, []) end)
-      |> Enum.filter(fn
-        [%{language: language} | _] ->
-          language == sim.language.iso_639_3
-
-        parameter ->
-          parameter
-      end)
+      |> Enum.filter(fn [%{language: language} | _] -> language == sim.language.iso_639_3 end)
       |> Enum.reverse()
       |> List.flatten()
       |> Enum.map(fn %{payload: payload} -> payload end)
