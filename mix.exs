6601d4fa8f519f6b0f84b0626775782d45aa671c
Arjen <aratramba@gmail.com>
2025-09-15 18:00:25 +0200

    update expression and bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule FlowRunner.MixProject do
   use Mix.Project
 
-  @version "6.3.1"
+  @version "6.3.2"
 
   def project do
     [
@@ -57,7 +57,7 @@ defmodule FlowRunner.MixProject do
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:elixir_uuid, "~> 1.2"},
       {:excoveralls, "~> 0.10", only: :test},
-      {:expression, "~> 2.47.1"},
+      {:expression, "~> 2.47.2"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:finch, "~> 0.17"},
       {:iptools, "~> 0.0.5"},
